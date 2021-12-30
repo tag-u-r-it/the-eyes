a8c9865019cfb46117e6b5c2bfb3849ae8f98814
tag-u-r-it <lauri.talvipuro@protonmail.com>
2021-12-30 13:22:51 +0200

    left eye

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Drawing;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
@@ -25,8 +26,11 @@ namespace the_eyes
             line_max.Stroke = new SolidColorBrush(Windows.UI.Colors.Green);
             line_max.X1 = left_eye.ActualOffset.X + left_eye.Width / 2;
             line_max.Y1 = left_eye.ActualOffset.Y + left_eye.Height / 2;
+            line_max2.X1 = right_eye.ActualOffset.X + right_eye.Width / 2;
+            line_max2.Y1 = right_eye.ActualOffset.Y + right_eye.Height / 2;
 
-            //line2.Stroke = new SolidColorBrush(Windows.UI.Colors.Red);
+            line2.Stroke = new SolidColorBrush(Windows.UI.Colors.Red);
+            line_max2.Stroke = new SolidColorBrush(Windows.UI.Colors.Green);
             line2.X1 = right_eye.ActualOffset.X + right_eye.Width / 2;
             line2.Y1 = right_eye.ActualOffset.Y + right_eye.Height / 2;
 
@@ -36,8 +40,7 @@ namespace the_eyes
 
         }
 
-        static bool isInside(double circle_x, double circle_y,
-                                  double rad, double x, double y)
+        static bool isInside(double circle_x, double circle_y, double rad, double x, double y)
         {
             if ((x - circle_x) * (x - circle_x) + (y - circle_y) * (y - circle_y) <= rad * rad)
                 return true;
@@ -45,6 +48,12 @@ namespace the_eyes
                 return false;
         }
 
+        public static double GetAngleOfLineBetweenTwoPoints(PointF p1, PointF p2)
+        {
+            float xDiff = p2.X - p1.X;
+            float yDiff = p2.Y - p1.Y;
+            return Math.Atan2(yDiff, xDiff) * (180 / Math.PI);
+        }
         private async void refresh_sight(object sender, RoutedEventArgs e)
         {
             while (true)
@@ -55,15 +64,7 @@ namespace the_eyes
                 //left eye
                 line.X2 = pointerPosition.X - Window.Current.Bounds.X;
                 line.Y2 = pointerPosition.Y - Window.Current.Bounds.Y;
-                left_iris.CenterPoint = new System.Numerics.Vector3((float)line.X2, (float)line.Y2, 0.0f);
-                //if (line.X2 < left_eye.ActualOffset.X-15 + left_eye.Width && line.X2 > left_eye.ActualOffset.X)
-                //{
-                //    Canvas.SetLeft(left_iris, line.X2);
-                //}
-                //if (line.Y2 < left_eye.ActualOffset.Y-15 + left_eye.Height && line.Y2 > left_eye.ActualOffset.Y)
-                //{
-                //    Canvas.SetTop(left_iris, line.Y2);
-                //}
+
                 double circle_x = left_eye.ActualOffset.X + left_eye.Width / 2;
                 double circle_y = left_eye.ActualOffset.Y + left_eye.Height / 2;
                 double x = pointerPosition.X - Window.Current.Bounds.X;
@@ -78,20 +79,46 @@ namespace the_eyes
                 }
                 else
                 {
-
-                    debug.Text = (line.X2 - line.X1).ToString();
+                    PointF p1 = new PointF((float)line.X1, (float)line.Y1);
+                    PointF p2 = new PointF((float)line.X2, (float)line.Y2);
+                    double angle = GetAngleOfLineBetweenTwoPoints(p1, p2);
+                    angle = angle - 90;
+                    double x1 = line.X1 + rad * Math.Sin(-angle * Math.PI / 180f);
+                    double y1 = line.Y1 + rad * Math.Cos(-angle * Math.PI / 180f);
+                    line_max.X2 = x1;
+                    line_max.Y2 = y1;
+                    Canvas.SetLeft(left_iris, line_max.X2);
+                    Canvas.SetTop(left_iris, line_max.Y2);
                 }
 
                 //right eye
                 line2.X2 = pointerPosition.X - Window.Current.Bounds.X;
                 line2.Y2 = pointerPosition.Y - Window.Current.Bounds.Y;
-                if (line2.X2 < right_eye.ActualOffset.X - 15 + right_eye.Width && line2.X2 > right_eye.ActualOffset.X)
+
+                circle_x = right_eye.ActualOffset.X + right_eye.Width / 2;
+                circle_y = right_eye.ActualOffset.Y + right_eye.Height / 2;
+                x = pointerPosition.X - Window.Current.Bounds.X;
+                y = pointerPosition.Y - Window.Current.Bounds.Y;
+                rad = right_eye.Width / 2;
+                if (isInside(circle_x, circle_y, rad, x, y))
                 {
+                    line_max2.X2 = pointerPosition.X - Window.Current.Bounds.X;
+                    line_max2.Y2 = pointerPosition.Y - Window.Current.Bounds.Y;
                     Canvas.SetLeft(right_iris, line2.X2);
-                }
-                if (line2.Y2 < right_eye.ActualOffset.Y - 15 + right_eye.Height && line2.Y2 > right_eye.ActualOffset.Y)
-                {
                     Canvas.SetTop(right_iris, line2.Y2);
+                }
+                else
+                {
+                    PointF p1 = new PointF((float)line.X1, (float)line.Y1);
+                    PointF p2 = new PointF((float)line.X2, (float)line.Y2);
+                    double angle = GetAngleOfLineBetweenTwoPoints(p1, p2);
+                    angle = angle - 90;
+                    double x1 = line2.X1 + rad * Math.Sin(angle * Math.PI / 180f);
+                    double y1 = line2.Y1 + rad * Math.Cos(angle * Math.PI / 180f);
+                    line_max2.X2 = x1;
+                    line_max2.Y2 = y1;
+                    Canvas.SetLeft(right_iris, line_max2.X2);
+                    Canvas.SetTop(right_iris, line_max2.Y2);
                 }
                 Main_canvas.UpdateLayout();
             }
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 UWP test. Eyes will follow mouse cursor.
 
 ## TODO
-* The eyes are moving in square-ish manner. Make it full circle
-* The eye should be an object. Create a class(?) for it.
+* Iris offset needs to be fixed
+* Right eye is not feeling too well
